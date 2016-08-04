f474b861862348a75c2eb5134af2a96d90c20956
Mike Walker <michael@lazerwalker.com>
2016-08-04 12:13:57 -0400

    Randomly adjust voice pitch

--- Poetry/RobotVoiceOutput.swift
+++ Poetry/RobotVoiceOutput.swift
@@ -13,6 +13,11 @@ class RobotVoiceOutput:NSObject, AVSpeechSynthesizerDelegate {
     func speak(text:String) {
         let utterance = AVSpeechUtterance(string:text)
         utterance.voice = AVSpeechSynthesisVoice(language: "en-IE")
+
+        let pitchModulator = (Float(arc4random_uniform(30))/100.0 - 0.15)
+        utterance.pitchMultiplier = 1.0 + pitchModulator
+
+        print("Playing at pitch \(utterance.pitchMultiplier)")
         synthesizer.speakUtterance(utterance)
     }
 
